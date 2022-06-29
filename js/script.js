2e88f172320aae6e93d84d3ca18ea80acdd32485
HerHeri <madeheri14@gmail.com>
2022-06-29 16:02:36 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,137 @@
+const todos         = [];
+const RENDER_EVENT  = 'render-todo';
+
+document.addEventListener(RENDER_EVENT, function(){
+    const uncompletedTODOList       = document.getElementById('todos');
+    uncompletedTODOList.innerHTML   = '';
+
+    const completedTODOList         = document.getElementById('completed-todos');
+    completedTODOList.innerHTML     = '';
+
+    for (const todoItem of todos) {
+        const todoElement   = makeTodo(todoItem);
+        if (!todoItem.isCompleted){
+            uncompletedTODOList.append(todoElement);
+            console.log('uncompleted');
+        }else{
+            completedTODOList.append(todoElement);
+            console.log('completed');
+        }
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const submitForm    = document.getElementById('form');
+    submitForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        addTodo();
+    });
+});
+
+// 1. Menambah ToDo
+function addTodo() {
+    const textTodo      = document.getElementById('title').value;
+    const timestamp     = document.getElementById('date').value;
+
+    const generatedID   = generatedId();
+    const todoObject    = generateTodoObject(generatedID, textTodo, timestamp, false);
+    todos.push(todoObject);
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function generatedId() {
+    return +new Date();
+}
+
+function generateTodoObject(id, task, timestamp, isCompleted) {
+    return {
+        id,
+        task,
+        timestamp,
+        isCompleted
+    }
+}
+// 2. Memindahkan ToDo
+function makeTodo(todoObject) {
+    const textTitle         = document.createElement('h2');
+    textTitle.innerText     = todoObject.task;  
+
+    const textTimestamp     = document.createElement('p');
+    textTimestamp.innerText = todoObject.timestamp;
+
+    const textContainer     = document.createElement('div');
+    textContainer.classList.add('inner');
+    textContainer.append(textTitle, textTimestamp);
+
+    const container = document.createElement('div');
+    container.classList.add('item', 'shadow');
+    container.append(textContainer);
+    container.setAttribute('id', `todo-${todoObject.id}`);
+
+    if(todoObject.isCompleted){
+        const undoButton    = document.createElement('button');
+        undoButton.classList.add('undo-button');
+        undoButton.addEventListener('click', function () {
+            undoTaskFromCompleted(todoObject.id);
+        });
+
+        const trashButton   = document.createElement('button');
+        trashButton.classList.add('trash-button');
+        trashButton.addEventListener('click', function () {
+            removeTaskFromCompleted(todoObject.id);
+        });
+        container.append(undoButton, trashButton);
+    }else{
+        const checkButton   = document.createElement('button');
+        checkButton.classList.add('check-button');
+        checkButton.addEventListener('click', function(){
+            addTaskComplete(todoObject.id);
+            console.log('testCheck');
+        });
+        container.append(checkButton);
+    }
+
+    return container;
+}
+
+function addTaskComplete(todoId) {
+    const todoTarget        = findTodo(todoId);
+
+    if(todoTarget == null ) return;
+
+    todoTarget.isCompleted   = true;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findTodo(todoId) {
+    for (const todoItem of todos) {
+        if(todoItem.id === todoId){
+            return todoItem;
+        }
+    }
+    return null;
+}
+
+function findTodoIndex(todoId) {
+    for(const index in todos){
+        if (todos[index].id === todoId) {
+            return index;
+        }
+    }
+    return -1;
+}
+// 3. Menghapus ToDo
+function removeTaskFromCompleted(todoId) {
+    const   todoTarget  = findTodoIndex(todoId);
+    if (todoTarget  === -1) return;
+    todos.splice(todoTarget, 1);
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function undoTaskFromCompleted(todoId) {
+    const todoTarget        = findTodo(todoId);
+    if (todoTarget == null) return;
+    todoTarget.isCompleted  = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
